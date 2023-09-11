6409d6318a98fae9b065117d28e793158cf60540
Anyikabs <114746509+Anyikabs@users.noreply.github.com>
2023-09-11 17:23:11 +0000

    Update 33 files

--- src/components/common/Heading.jsx
+++ src/components/common/Heading.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 
-const Heading = ({ title, subtitle }) => {
+const Heading = ({ title, title2, subtitle }) => {
   return (
     <>
       <div className='heading'>
         <h1>{title}</h1>
+        <h1 style={{ color: '#070707' }}>{title2}</h1>
         <p>{subtitle}</p>
       </div>
     </>
--- src/components/home/location/Location.jsx
+++ src/components/home/location/Location.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Heading from "../../common/Heading"
+
 import { location } from "../../data/Data"
 import "./style.css"
 
@@ -8,7 +9,7 @@ const Location = () => {
     <>
       <section className='location padding'>
         <div className='container'>
-          <Heading title='Explore By Location' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.' />
+          <Heading class='dark' title='Explore By Location' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.' />
 
           <div className='content grid3 mtop'>
             {location.map((item, index) => (
